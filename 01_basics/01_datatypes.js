dafd49e8a6202575ee35239837f9cbc75779d4a8
Priyanshu Bharti <154883348+Priyanshubhartistm@users.noreply.github.com>
2024-04-22 15:37:01 +0000

    basic datatype

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+//alert (3+3) //we are using nodejs,not browser
+
+console.log(
+    
+    3) //code readability should be high
+
+    console.log("Priyanshu")
+
+
+    let name = "Priyanshu"
+    let age = 19
+    let isLoggedIn = false
+
+
+    // number => 2 to pwer 53
+    // bignit it is used of big no. example:- trading,stock market,big websites like facebook
+    // string => single quotes,double quotes ' ' ,""
+    // bollean => true/false
+    // null standalone value
+    // undefined => now value are not assign/defined
+    // symbol => unique
+
+
+    // object
+
+    console.log(typeof null); //output object
+    console.log(typeof undefined); // it is output are undefined
