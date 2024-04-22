dbfe2335c151507371662065a922bb9acaf45439
Priyanshu Bharti <154883348+Priyanshubhartistm@users.noreply.github.com>
2024-04-22 15:36:16 +0000

    basic variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144533
+let accountEmail = "bharti@gmail.com"
+var accountpassword ="123"
+accountCity  = "patna"
+let accountstate;
+
+// accountId=2// not allowed
+
+accountEmail = "bharti@gmail.com"
+accountpassword = "2112"
+accountCity = "patna"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountstate])
---
+++ 01_basics/conversion_datatype.js
@@ -0,0 +1,27 @@
+let score = "priyanshu"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+
+
+// "33" =>33
+// "22abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1  => true;   0 => false
+// "" => false
+// "priyanshu" => false
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
